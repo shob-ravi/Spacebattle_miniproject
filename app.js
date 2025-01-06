c38d14cd6976c1c591c7a5dd09be7163f4a1b4b0
Shobana Hariraman <shobanaravi2004@gmail.com>
2025-01-05 22:44:23 -0500

    added logic for progress bar

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const containerEl = document.querySelector(".container");
-const containerEl_height = containerEl.offsetHeight;
-const containerEl_width = containerEl.offsetWidth;
-//console.log(containerEl_height,containerEl_width);
+let currentAlien = 0;
+const humanshipHealthEl = document.getElementById("humanshipHealth");
+const alienHealthEl = document.getElementById("alienHealth");
+const alienName = document.getElementById("alienName");
+const statusEl = document.getElementById("status");
+const logEl = document.getElementById("log");
+const startBtn = document.getElementById("startBtn");
 
 const player1Image = document.getElementById("player1_img");
 player1Image.width = 100;
@@ -13,6 +17,7 @@ class spaceShip {
         this.hull = hull;
         this.firepower = firepower;
         this.accuracy = accuracy;
+        this.maxHull = hull;
 
     }
     attack(input) {
@@ -75,7 +80,7 @@ function startGame() {
         while (!alien.IsShipDestroyed() && !humanplayer1.IsShipDestroyed()) {
             humanplayer1.attack(alien);
             if (alien.IsShipDestroyed()) {
-                
+
                 console.log(`${alien.name} has been destroyed!`);
                 break;
             }
@@ -89,4 +94,16 @@ function startGame() {
         }
     }
 }
-startGame();
+// Update health bars
+function updateProgressBars() {
+    humanshipHealthEl.style.width = `${(humanplayer1.hull / humanplayer1.maxHull) * 100}%`;
+    alienHealthEl.style.width = `${(alienShip_array[currentAlien].hull / alienShip_array[currentAlien].maxHull) * 100}%`;
+  }
+
+
+// Update the status message
+function updateStatus(message) {
+    statusEl.innerText = message;
+  }
+
+  startGame();
